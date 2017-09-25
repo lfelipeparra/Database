e457dd2de6ddefa08eb518582d9a373338691ea1
lfelipe.parra <lfelipe.parra@udea.edu.co>
2017-09-25 17:50:15 -0500

    ExpandibleListView

--- app/src/main/java/com/parra/lfelipe/database/ListaLugares.java
+++ app/src/main/java/com/parra/lfelipe/database/ListaLugares.java
@@ -1,19 +1,17 @@
 package com.parra.lfelipe.database;
 
 import android.content.Context;
-import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.ExpandableListView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ListaLugares extends AppCompatActivity {
@@ -21,8 +19,14 @@ public class ListaLugares extends AppCompatActivity {
     private String consulta;
     private List<objLugar> lugares;
     private List<String> nombres;
-    private ListView listview;
+    private ExpandableListView listview;
+    //private ImageView img;
     private int posConsulta;
+    private String[] padres = new String[50];;
+    private String[][] hijos = new String[50][50];
+    private int[][] idHijos = new int[50][50];
+    private int[] cntHijos = new int[50];
+    private int[] idPadres = new int[50];
 
     public static final String TABLE_NAME = "Cuentenos";
     public static final String COLUMN_NAME_ID = "id";
@@ -33,7 +37,7 @@ public class ListaLugares extends AppCompatActivity {
     public static final String COLUMN_NAME_TIPO = "tipo";
 
 
-    public class objLugar{
+    /*public class objLugar{
         private String cuidad, nombre, telefono, direccion, tipo;
         public objLugar(String cuidad, String nombre, String telefono, String direccion, String tipo){
             this.cuidad = cuidad;
@@ -42,7 +46,17 @@ public class ListaLugares extends AppCompatActivity {
             this.direccion = direccion;
             this.tipo = tipo;
         }
+    }*/
 
+    public boolean buscar(String compare){
+        posConsulta = 0;
+        for(String nombre:nombres){
+            if(compare.equals(nombre)){
+                return true;
+            }
+            posConsulta++;
+        }
+        return false;
     }
 
     public class FeedReaderDbHelper extends SQLiteOpenHelper {
@@ -69,6 +83,28 @@ public class ListaLugares extends AppCompatActivity {
         }
     }
 
+    public static String[] cleanArray(final String[] v) {
+        List<String> list = new ArrayList<String>(Arrays.asList(v));
+        list.removeAll(Collections.singleton(null));
+        return list.toArray(new String[list.size()]);
+    }
+
+    public static String[][] cleanMatriz(final String[][] s) {
+        String[][] s1 = new String[s.length][];
+        int k = 0;
+
+        for (int i = 0; i < s.length; i++) {
+            ArrayList<Object> list = new ArrayList<Object>();
+            for (int j = 0; j < s[i].length; j++) {
+                if (s[i][j] != null) {
+                    list.add(s[i][j]);
+                }
+            }
+            s1[k++] = list.toArray(new String[list.size()]);
+        }
+        return s1;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -77,7 +113,10 @@ public class ListaLugares extends AppCompatActivity {
         consulta = extras.getString("consulta");
         lugares = new ArrayList<objLugar>();
         nombres = new ArrayList<String>();
-        listview = (ListView) findViewById(R.id.listView);
+        listview = (ExpandableListView) findViewById(R.id.listView);
+        /*img = (ImageView) findViewById(R.id.imagen);
+        Picasso.with(ListaLugares.this).load("http://i.imgur.com/DvpvklR.png").into(img);*/
+
         Log.i("consulta",consulta);
 
         FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(getApplication());
@@ -111,6 +150,8 @@ public class ListaLugares extends AppCompatActivity {
         );
 
 
+        int i = 0;
+        int j = 0;
         while(cursor.moveToNext()) {
             String nombre = cursor.getString(cursor.getColumnIndexOrThrow("nombre"));
             String cuidad = cursor.getString(cursor.getColumnIndexOrThrow("cuidad"));
@@ -118,25 +159,41 @@ public class ListaLugares extends AppCompatActivity {
             String direccion = cursor.getString(cursor.getColumnIndexOrThrow("direccion"));
             String tipo = cursor.getString(cursor.getColumnIndexOrThrow("tipo"));
             lugares.add(new objLugar(cuidad, nombre, telefono, direccion,tipo));
+            if(buscar(nombre)){
+                hijos[idPadres[posConsulta]][cntHijos[idPadres[posConsulta]]] = nombre;
+                idHijos[idPadres[posConsulta]][cntHijos[idPadres[posConsulta]]] = j;
+                idPadres[j] = idPadres[posConsulta];
+                cntHijos[idPadres[posConsulta]]++;
+            }else{
+                padres[i] = nombre;
+                hijos[i][0] = nombre;
+                idHijos[i][0] = j;
+                idPadres[j] = i;
+                System.out.println(j);
+                cntHijos[i] = 1;
+                i++;
+            }
             nombres.add(nombre);
+            j++;
         }
         cursor.close();
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,nombres);
+        ExpandibleListViewAdapter adapter = new ExpandibleListViewAdapter(ListaLugares.this, cleanArray(padres), cleanMatriz(hijos),lugares,idHijos);
         listview.setAdapter(adapter);
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        /*listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String nombre = lugares.get(i).nombre;
-                String cuidad = lugares.get(i).cuidad;
-                String telefono = lugares.get(i).telefono;
-                String direccion = lugares.get(i).direccion;
+                String nombre = lugares.get(idHijos[i][0]).nombre;
+                String cuidad = lugares.get(idHijos[i][0]).cuidad;
+                String telefono = lugares.get(idHijos[i][0]).telefono;
+                String direccion = lugares.get(idHijos[i][0]).direccion;
                 Intent intent = new Intent(ListaLugares.this,infoLugar.class);
                 intent.putExtra("nombre",nombre);
                 intent.putExtra("cuidad",cuidad);
                 intent.putExtra("telefono",telefono);
                 intent.putExtra("direccion",direccion);
-                startActivity(intent);
+                startActivity(intent);fd
             }
-        });
+        });*/
     }
+
 }
